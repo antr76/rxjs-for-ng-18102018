d2b2017515be655dc8921407bbf0ebf8c46bde5f
Anatoli Trockmann <anatoli@Anatolis-MBP.fritz.box>
2018-11-10 01:27:40 +0100

    code improvement after review from the trainer

--- src/app.ts
+++ src/app.ts
@@ -17,70 +17,52 @@ import {
 // Do not do it in the index.html it will not work!
 import './styles.css';
 
-const inputElement = <Element>document.querySelector('#input');
-
+const inputElement = document.querySelector('#input') as Element;
 const input$ = fromEvent(inputElement, 'input');
 
-const githubRepos$: Observable<GithubRepository[]> = input$
-    .pipe(
-        map(mapEventToValue),
+searchGithubRepos(input$).subscribe(renderData);
+
+function searchGithubRepos(input$: Observable<Event>): Observable<GithubRepository[]> {
+    return input$.pipe(
+        pluck<Event, string>('target', 'value'),
         filter(isNotEmpty),
         debounceTime(500),
         distinctUntilChanged(),
-        switchMap(search => loadFromGithub(search)),
+        switchMap(loadFromGithub),
         tap(console.log)
     );
-
-githubRepos$.subscribe(
-    (githubRepos: GithubRepository[]) => renderData(githubRepos)
-);
+}
 
 function renderData(githubRepos: GithubRepository[]): void {
-    const parentElement = <Element>document.querySelector('#output');
+    const parentElement = document.querySelector('#output') as Element;
     // Remove all child elements from that element
     // After that happened new children will be added.
     parentElement.innerHTML = '';
-    githubRepos.forEach((githubRepo: GithubRepository) => {
-        renderRepoInfo(parentElement as Element, githubRepo);
-    });
+    githubRepos.forEach(
+        githubRepo => renderRepoInfo(parentElement, githubRepo)
+    );
 }
 
-function renderRepoInfo(parentElement: Element, repo: GithubRepository) {
-    const divElement = document.createElement('div');
-    divElement.className = 'repo-wrapper';
-    // info element
-    const infoElement = document.createElement('p');
-    infoElement.className = 'info';
-    infoElement.innerHTML = repo.name;
-    divElement.appendChild(infoElement);
-    // url element
-    const urlElement = document.createElement('p');
-    const ancorElement = document.createElement('a');
-    const url = repo.url;
-    ancorElement.setAttribute("href", url);
-    ancorElement.setAttribute("target", '_blank');
-    ancorElement.innerHTML = url;
-    urlElement.appendChild(ancorElement);
-    urlElement.className = 'url';
-    divElement.appendChild(urlElement);
-    // description element
-    const descriptionElement = document.createElement('p');
-    descriptionElement.className = 'description';
-    descriptionElement.innerHTML = repo.description;
-    divElement.appendChild(descriptionElement);
-    parentElement.append(divElement);
+function renderRepoInfo(parentElement: Element, repo: GithubRepository): void {
+    const wrapperElement = document.createElement('div') as Element;
+    parentElement.append(wrapperElement);
+    wrapperElement.className = 'repo-wrapper';
+    const wrapperElementContent = `
+        <p class="info">${repo.name}</p>
+        <p class="url">
+            <a href="${repo.url}" target="_blank">${repo.url}</a>
+        </p>
+        <p class="description">${repo.description}</p>
+    `;
+    wrapperElement.innerHTML = wrapperElementContent;
 }
 
 function isNotEmpty(value: string) {
     return value.length > 0;
 }
 
-function mapEventToValue(event: any): string {
-    return event.target.value;
-}
-
-function mapToGithubRepository(items: any): GithubRepository[] {
-    return items.map((item: any) => new GithubRepository(item));
+function mapToGithubRepository(items: GithubRepositoryInfo[]): GithubRepository[] {
+    return items.map(item => new GithubRepository(item));
 }
 
 function loadFromGithub(search: string): Observable<GithubRepository[]> {
@@ -88,14 +70,14 @@ function loadFromGithub(search: string): Observable<GithubRepository[]> {
 
     return fetchData(url)
         .pipe(
-            pluck('items'),
+            pluck<{}, GithubRepositoryInfo[]>('items'),
             //tap(data => console.log('before map', data)),
             map(mapToGithubRepository),
             //tap(data => console.log('after map', data)),
         );
 }
 
-function fetchData(url: string) {
+function fetchData(url: string): Observable<GithubRepositoryInfo> {
     return from(
         fetch(url).then(response => response.json())
     );
@@ -107,9 +89,9 @@ class GithubRepository {
     public description: string;
     public url: string;
     public forks: number;
-    public stars: string;
+    public stars: number;
 
-    constructor(data: any) {
+    constructor(data: GithubRepositoryInfo) {
         this.name = data['name'];
         this.homepage = data['homepage'];
         this.description = data['description'];
@@ -117,5 +99,14 @@ class GithubRepository {
         this.forks = data['forks'];
         this.stars = data['stargazers_count'];
     }
+}
 
+// Type which describes the original data structure of the github repository
+type GithubRepositoryInfo = {
+    name: string
+    homepage: string
+    description: string
+    html_url: string
+    forks: number
+    stargazers_count: number
 }
